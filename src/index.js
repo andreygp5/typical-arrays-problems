cd77832154a340edfd6c28338284a4b989e67f4b
andreygp5 <andreygp555@gmail.com>
2021-01-24 22:43:33 +0300

    feat: add avg, max, min array functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (array) {
+        return array.reduce((min, value) => (min = Math.min(min, value)), 0);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array) {
+        return array.reduce((max, value) => (max = Math.max(max, value)), 0);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length != 0) {
+        return array.reduce((sum, value) => (sum += value), 0) / array.length;
+    } else {
+        return 0;
+    }
+};
